e8bdadb68c338259695e52495e1a5cb5cf906d2a
Manu <16203442+manuelex20@users.noreply.github.com>
2023-06-20 03:41:50 -0500

    detección de plagas

--- detector.py
+++ detector.py
@@ -1,9 +1,9 @@
 import cv2
 
 
-cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)  # 0 = cámara por default. Si
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)  # 0 = cámara por default. Si
 
-modeloHuevos = cv2.CascadeClassifier("huevosSanos.xml")
+modeloHuevos = cv2.CascadeClassifier("imagenes/queresa/queresa.xml")
 
 while True:
 
@@ -12,16 +12,18 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     toy = modeloHuevos.detectMultiScale(
-        gray, scaleFactor=3.5, minNeighbors=1700, minSize=(35, 35)  # 5  # 3500
+        gray, scaleFactor=4, 
+        minNeighbors=630,
+        minSize=(80,80)  # 5  # 3500
     )
 
     for (x, y, w, h) in toy:
 
         cv2.rectangle(
-            img=frame, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2
+            img=frame, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=1
         )
 
-        cv2.putText(frame, "Huevo", (x, y - 10), 2, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame, "arania", (x, y - 10), 2, 0.7, (0, 255, 0), 2, cv2.LINE_AA)
 
     cv2.imshow("frame", frame)
 
--- recoletarImagenes.py
+++ recoletarImagenes.py
@@ -3,11 +3,11 @@ import numpy as np
 import imutils
 import os
 
-Datos = "p"
+Datos = "n"
 if not os.path.exists(Datos):
     print("Carpeta creada: ", Datos)
     os.makedirs(Datos)
-cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 x1, y1 = 190, 80
 x2, y2 = 450, 398
 count = 0
